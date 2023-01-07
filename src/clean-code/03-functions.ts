32b8a9806ffe213b76893474072fa059de3c4387
Juan David Marcillo Alba <jdm.a0125@gmail.com>
2023-01-07 18:08:36 -0500

    Name functions regarding what we expect it does, also it's recommended to see how many params the function receive and figure out if we should use an object or not, the most of the cases we should use an object if the function receive more than three params.

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,38 @@
+(() => {
+  // función para obtener información de una película por Id
+  function getMovieById(id: string) {
+    console.log({ id });
+  }
+
+  // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+  function getMovieCastById(movieId: string) {
+    console.log({ movieId });
+  }
+
+  // funcion para obtener el bio del actor por el id
+  function getActorBioById(actorId: string) {
+    console.log({ actorId });
+  }
+
+  interface Movie {
+    cast: string[];
+    description: string;
+    rating: number;
+    title: string;
+  }
+  // Crear una película
+  function createMovie({ title, description, rating, cast }: Movie) {
+    console.log({ title, description, rating, cast });
+  }
+
+  // Crea un nuevo actor
+  function createActor(fullName: string, birthdate: Date): boolean {
+    // tarea asincrona para verificar nombre
+    // ..
+    // ..
+    if (fullName === "fernando") return false;
+
+    console.log("Crear actor", birthdate);
+    return true;
+  }
+})();
